5911e5c4cffdd0cffa6725567fc1ef560c787205
tuan.nguyen4 <tuan.nguyen4@asiantech.vn>
2020-07-21 16:19:09 +0700

    total cart and covention

--- js/cart.js
+++ js/cart.js
@@ -3,28 +3,11 @@ var cartTable = document.querySelector('table#cart-table tbody');
 var inputQtyCarts = cartTable.getElementsByClassName('cart-qty');
 var deleteCart = cartTable.getElementsByClassName('cart-remove-one');
 var btnDeleteAll = document.querySelector('button#remove-all');
-// document.addEventListener('DOMContentLoaded', getCartFromLocalStorage);
-if (localStorage.getItem('carts') === null) {
-    carts = [];
-} else {
-    carts = JSON.parse(localStorage.getItem('carts'));
-}
-
-if (carts !== null) {
-    carts.forEach(element => {
-        cartTable.innerHTML += `
-            <tr class="row-cart">
-                <td>${element['name']}</td>
-                <td><img class="cart-img" src="images/${element['image']}" alt=""></td>
-                <td>${element['price']}</td>
-                <td><input class="cart-qty" type="number" min="0" max="10" data_id = ${element['id']} value="${element['qty']}"></td>
-                <td><button class="cart-remove-one" data_id = ${element['id']}>delete</button></td>
-            </tr>
-        `
-    });
-}
+var totalOutput = document.getElementById('total');
 
 document.addEventListener('DOMContentLoaded', function () {
+    getCartFromLocalStorage();
+    getTotalCart();
     //add event to input qty
     for (let i = 0; i < inputQtyCarts.length; i++) {
         inputQtyCarts[i].addEventListener('change', setQtyService);
@@ -39,58 +22,61 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 function setQtyService(e) {
-    var id = e.target.getAttribute('data_id');
-    var qty = Number(e.target.value);
+    let id = e.target.getAttribute('data_id');
+    let qty = Number(e.target.value);
+    let newCarts;
 
-    if (qty > 10 || qty < 0) {
+    if (qty > 20 || qty < 0)
         alert("the qty must be between 0 and 10");
-        e.target.setAttribute('value', e.target.getAttribute('value'));
-    }
-    console.log(id, qty);
-    if (qty <= 0) {
+    if (qty > 20)
+        location.reload();
+    if (qty <= 0)
         e.target.parentElement.parentElement.remove();
-    }
-    var newCarts = setQtyCart(carts, id, qty);
+
+    newCarts = setQtyCart(carts, id, qty);
     localStorage.setItem('carts', JSON.stringify(newCarts));
+    getTotalCart();
 }
 
 function deleteCartfunction(e) {
-    var id = e.target.getAttribute('data_id');
-    // console.log(id);
-    var newCart = removeCartById(carts, id);
-    console.log(newCart);
+    let id = e.target.getAttribute('data_id');
+    let newCart;
+
     e.target.parentElement.parentElement.remove();
+    newCart = removeCartById(carts, id);
     localStorage.setItem('carts', JSON.stringify(newCart));
+    getTotalCart();
 }
 
 function deleteAllCart(e) {
     localStorage.removeItem('carts');
+    carts = [];
     cartTable.querySelectorAll('tr.row-cart').forEach(element => {
         element.remove();
     });
+    getTotalCart();
 }
 
 
 function removeCartById(arrCart, idDelete) {
-    var i = arrCart.length;
-    while (i--) {
-        if (arrCart[i]['id'] === idDelete) {
+    for (let i = 0; i < arrCart.length; i++) {
+        if (arrCart[i]['id'] === idDelete) 
             arrCart.splice(i, 1);
-        }
     }
+
     return arrCart;
 }
 
 function setQtyCart(arrCart, id, qty) {
     for (let i = 0; i < arrCart.length; i++) {
-        if (arrCart[i].id != id) 
-            continue;            
-        if (qty<=0) {
+        if (arrCart[i].id != id)
+            continue;
+        if (qty <= 0) {
             arrCart.splice(i, 1);
             break;
         }
-        if (qty > 10) 
-            qty = 10;
+        if (qty > 20)
+            break;
 
         arrCart[i].qty = qty;
     }
@@ -98,3 +84,32 @@ function setQtyCart(arrCart, id, qty) {
     return arrCart;
 }
 
+function getCartFromLocalStorage() {
+    if (localStorage.getItem('carts') === null) {
+        carts = [];
+    } else {
+        carts = JSON.parse(localStorage.getItem('carts'));
+    }
+
+    if (carts !== null) {
+        carts.forEach(element => {
+            cartTable.innerHTML += `
+                <tr class="row-cart">
+                    <td>${element['name']}</td>
+                    <td><img class="cart-img" src="images/${element['image']}" alt=""></td>
+                    <td>${element['price']}</td>
+                    <td><input class="cart-qty" type="number" min="0" max="20" data_id = ${element['id']} value="${element['qty']}"></td>
+                    <td><button class="cart-remove-one" data_id = ${element['id']}>delete</button></td>
+                </tr>
+            `
+        });
+    }
+}
+
+function getTotalCart() {
+    let total = 0;
+    carts.forEach(element => {
+        total += Number(element.qty) * Number(element.price);
+    });
+    totalOutput.innerText = total;
+}
--- js/js.js
+++ js/js.js
@@ -1,96 +1,95 @@
 var listService = document.getElementsByClassName('service-list')[0];
 var btnAddCarts = document.getElementsByClassName('btn-add-cart');
 var carts;
-if(localStorage.getItem('carts') === null){
-    carts = [];
-} else {
-    carts = JSON.parse(localStorage.getItem('carts'));
-}
-
 var services = [
     {
         id: 1,
-        name: "dda",
+        name: "neck shoulder pain",
         content: "asdasdas",
         image: 'img_number_1.jpg',
         price: 1000
     },
     {
         id: 2,
-        name: "asdasd",
+        name: "neck shoulder pain",
         content: "asdasdas",
         image: 'img_number_2.jpg',
         price: 2000
     },
     {
         id: 3,
-        name: "asdasd",
+        name: "neck shoulder pain",
         content: "asdasdas",
         image: 'img_number_3.jpg',
         price: 1000
     },
     {
         id: 4,
-        name: "asdasd",
+        name: "neck shoulder pain",
         content: "asdasdas",
         image: 'img_number_4.jpg',
         price: 1000
     },
     {
         id: 5,
-        name: "asdasd",
+        name: "neck shoulder pain",
         content: "asdasdas",
         image: 'img_number_5.jpg',
         price: 3000
     },
     {
         id: 6,
-        name: "asdasd",
+        name: "slow say",
         content: "asdasdas",
         image: 'img_number_6.jpg',
         price: 5000
+    },
+    {
+        id: 7,
+        name: "slow say",
+        content: "asdasdas",
+        image: 'img_number_7.jpg',
+        price: 1000
+    },
+    {
+        id: 8,
+        name: "neck shoulder pain",
+        content: "asdasdas",
+        image: 'img_number_8.jpg',
+        price: 3000
+    },
+    {
+        id: 9,
+        name: "slow say",
+        content: "asdasdas",
+        image: 'img_number_9.jpg',
+        price: 5000
     }
 ];
- 
-var contentServices = '';
-for(var i = 0; i<services.length; i++){
-    contentServices += `
-        <li class="service-item">
-            <a href="" class="service-inner">
-                <img class="service-img" src="images/${services[i]['image']}" alt="">
-                <h3>${services[i]['name']}</h3>
-                <p class="content-service">${services[i]['content']}</p>
-                <p>Price: <span class="price-service">${services[i]['price']}</span></p>
-            </a>
-            <button class ="btn-add-cart" data_id ="${services[i]['id']}" >add cart</button>
-        </li>
-    `;
-}
-listService.innerHTML=contentServices;
 
+document.addEventListener('DOMContentLoaded', function () {
+    getCart();
 
-// add event click to service
-for (let i = 0; i < btnAddCarts.length; i++) {
-    const btnAddCart = btnAddCarts[i];
-    btnAddCart.addEventListener('click',buyService);
-}
+    getServices();
+    // add event click to service
+    for (let i = 0; i < btnAddCarts.length; i++) {
+        btnAddCarts[i].addEventListener('click', buyService);
+    }
 
-
-
+});
 
 // add card to storage
-function buyService(event){
-    // console.log(event.target);
-    var service = event.target.parentElement;
-    var serviceInfo = getInforService(service);
+function buyService(event) {
+    let service = event.target.parentElement;
+    let serviceInfo = getInforService(service);
+
     saveCart(serviceInfo);
 }
 
-function getInforService(service){
-    var serviceInfo = {
+function getInforService(service) {
+    let serviceInfo = {
         id: service.querySelector('button.btn-add-cart').getAttribute('data_id'),
         name: service.querySelector('h3').textContent,
-        content: service.querySelector('p.content-service').textContent,
         image: service.querySelector('img').src.match(/[\w-]+\.jpg/g)[0],
         price: service.querySelector('span.price-service').textContent,
         qty: 1
@@ -98,21 +97,44 @@ function getInforService(service){
     return serviceInfo;
 }
 
-function saveCart(serviceInfo){
-    var status = true;
+function saveCart(serviceInfo) {
+    let status = true;
     carts.forEach(cart => {
-        if (cart.id === serviceInfo.id) {
-            cart.qty +=1;
-            status = false;
+        if (cart.id !== serviceInfo.id)
+            return;
+        cart.qty += 1;
+        if (cart.qty >= 20) {
+            cart.qty = 20;
+            alert('so luong service phai duoi 20');
         }
+        status = false;
     });
 
-    if (status) {
+    if (status)
         carts.push(serviceInfo);
-    }
-    console.log(carts);
+
     localStorage.setItem('carts', JSON.stringify(carts));
 }
-//end add cart to storage;
 
+function getCart() {
+    if (localStorage.getItem('carts') === null) 
+        carts = [];
+    else 
+        carts = JSON.parse(localStorage.getItem('carts'));
+}
 
+function getServices() {
+    for (let i = 0; i < services.length; i++) {
+        listService.innerHTML += `
+            <li class="service-item">
+                <a href="" class="service-inner">
+                    <img class="service-img" src="images/${services[i]['image']}" alt="">
+                    <h3>${services[i]['name']}</h3>
+                    <p class="content-service">${services[i]['content']}</p>
+                    <p>Price: <span class="price-service">${services[i]['price']}</span></p>
+                </a>
+                <button class ="btn-add-cart" data_id ="${services[i]['id']}" >add cart</button>
+            </li>
+        `;
+    }
+}
