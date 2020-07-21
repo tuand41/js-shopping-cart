dac48da2061602c5df571d8e11e222e729af3db3
tuan.nguyen4 <tuan.nguyen4@asiantech.vn>
2020-07-21 22:06:34 +0700

    fix css

--- js/js.js
+++ js/js.js
@@ -5,63 +5,63 @@ var services = [
     {
         id: 1,
         name: "neck shoulder pain",
-        content: "asdasdas",
+        content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita, animisapiente.Et nesciunt natus delectus numquam quaerat fugiat dolorem? Inventore quasiprovident",
         image: 'img_number_1.jpg',
         price: 1000
     },
     {
         id: 2,
         name: "neck shoulder pain",
-        content: "asdasdas",
+        content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita, animisapiente.Et nesciunt natus delectus numquam quaerat fugiat dolorem? Inventore quasiprovident",
         image: 'img_number_2.jpg',
         price: 2000
     },
     {
         id: 3,
         name: "neck shoulder pain",
-        content: "asdasdas",
+        content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita, animisapiente.Et nesciunt natus delectus numquam quaerat fugiat dolorem? Inventore quasiprovident",
         image: 'img_number_3.jpg',
         price: 1000
     },
     {
         id: 4,
         name: "neck shoulder pain",
-        content: "asdasdas",
+        content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita, animisapiente.Et nesciunt natus delectus numquam quaerat fugiat dolorem? Inventore quasiprovident",
         image: 'img_number_4.jpg',
         price: 1000
     },
     {
         id: 5,
         name: "neck shoulder pain",
-        content: "asdasdas",
+        content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita, animisapiente.Et nesciunt natus delectus numquam quaerat fugiat dolorem? Inventore quasiprovident",
         image: 'img_number_5.jpg',
         price: 3000
     },
     {
         id: 6,
         name: "slow say",
-        content: "asdasdas",
+        content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita, animisapiente.Et nesciunt natus delectus numquam quaerat fugiat dolorem? Inventore quasiprovident",
         image: 'img_number_6.jpg',
         price: 5000
     },
     {
         id: 7,
         name: "slow say",
-        content: "asdasdas",
+        content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita, animisapiente.Et nesciunt natus delectus numquam quaerat fugiat dolorem? Inventore quasiprovident",
         image: 'img_number_7.jpg',
         price: 1000
     },
     {
         id: 8,
         name: "neck shoulder pain",
-        content: "asdasdas",
+        content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita, animisapiente.Et nesciunt natus delectus numquam quaerat fugiat dolorem? Inventore quasiprovident",
         image: 'img_number_8.jpg',
         price: 3000
     },
     {
         id: 9,
         name: "slow say",
-        content: "asdasdas",
+        content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita, animisapiente.Et nesciunt natus delectus numquam quaerat fugiat dolorem? Inventore quasiprovident",
         image: 'img_number_9.jpg',
         price: 5000
     }
@@ -80,8 +80,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
 // add card to storage
 function buyService(event) {
-    let service = event.target.parentElement;
+    let service = event.target.parentElement.parentElement;
     let serviceInfo = getInforService(service);
+    console.log(service);
 
     saveCart(serviceInfo);
 }
@@ -103,7 +104,7 @@ function saveCart(serviceInfo) {
         if (cart.id !== serviceInfo.id)
             return;
         cart.qty += 1;
-        if (cart.qty >= 20) {
+        if (cart.qty > 20) {
             cart.qty = 20;
             alert('so luong service phai duoi 20');
         }
@@ -129,11 +130,15 @@ function getServices() {
             <li class="service-item">
                 <a href="" class="service-inner">
                     <img class="service-img" src="images/${services[i]['image']}" alt="">
-                    <h3>${services[i]['name']}</h3>
-                    <p class="content-service">${services[i]['content']}</p>
-                    <p>Price: <span class="price-service">${services[i]['price']}</span></p>
+                    <div class="container-service">
+                        <h3>${services[i]['name']}</h3>
+                        <p class="content-service">${services[i]['content']}</p>
+                        <p>Price: <span class="price-service">${services[i]['price']}</span></p>
+                    </div>
                 </a>
-                <button class ="btn-add-cart" data_id ="${services[i]['id']}" >add cart</button>
+                <div class="container">
+                    <button class ="btn-add-cart" data_id ="${services[i]['id']}" >add cart</button>
+                </div>
             </li>
         `;
     }
